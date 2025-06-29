58560c004203506827c6ad948788872724a36cdf
Remus Daniel Blaj <remoblaj@gmail.com>
2025-06-30 00:23:29 +0200

    gestire input di testo per aggiornare lo stato

---
+++ exercise_project/src/TextInput.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const TextInput = () => {
+    const [text, setText] = useState("");
+};
+
+const handleChange = (event) => {
+    setText(event.target.value)
+};
+
+return (
+    <div>
+        <input type="text"  value={text}  onChange={handleChange} />
+    </div>
+)
+
+export default TextInput;

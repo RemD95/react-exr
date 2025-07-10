fa9171c78affd3e6bb8077c036868b03ef8c0d87
Remus Daniel Blaj <remoblaj@gmail.com>
2025-07-10 11:54:52 +0200

    rendere una lista di elementi

---
+++ exercise_project/src/ItemList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ItemList = (item) => {
+    return (
+        <>
+            <div>
+                <h2>Items List</h2>
+                <ul>
+                    {item.map((item, index) => (<li key={index}>
+                        {item}
+                    </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
+
+}

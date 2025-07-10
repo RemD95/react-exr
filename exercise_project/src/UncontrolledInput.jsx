cef0a8e675f4e6d2de0c8fe3ac8be421c25aa7ea
Remus Daniel Blaj <remoblaj@gmail.com>
2025-07-10 11:26:22 +0200

    usare la ref 3

--- exercise_project/src/UncontrolledInput.jsx
+++ exercise_project/src/UncontrolledInput.jsx
@@ -2,9 +2,17 @@ import { useRef } from "react";
 
 const UncontrolledInput = ()=>{
  const inputRef = useRef();
+ const handleButton = ()=> {
+  if(inputRef.current){
+    const inputValue = inputRef.current.value;
+    alert('Hai digitato : ${inputValue}')
+  }
+ }
    return(
         <>
         <input type="text"  ref={inputRef}/>
+
+        <button onClick={handleButton}>Accetta</button>
         </>
     )
 }

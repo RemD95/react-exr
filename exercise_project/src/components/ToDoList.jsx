55ad4c45b54e24b5da4e2ddb1b985cbb557c98cf
Remus Daniel Blaj <remoblaj@gmail.com>
2025-07-17 19:39:44 +0200

    FilteredTodoList

--- exercise_project/src/components/ToDoList.jsx
+++ exercise_project/src/components/ToDoList.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import useFetch from "../hooks/useFetch";
+import useFilteredTodos from "../hooks/usaFilteredTodos";
 
 const ToDoList = () => {
     const [text, setText] = useState("");
-    
+    const [searchTerm, setSearchTerm]=usestate("");  
     
         const [data, loading, error] = useFetch('https://jsonplaceholder.typicode.com/todos');
-       
+        const filteredTodos = useFilteredTodos(data, searchTerm);
         if (loading) 
             { return <div>Loading...</div> };
         if (error) 
@@ -16,10 +17,9 @@ const ToDoList = () => {
     return (
         <>
             <h2>To Do List</h2>
-             
-
+                <input type="text" onChange={(event)=>setSearchTerm(event.target.value)} />
                 <ul>
-                    {data.map(todo=>(
+                    {filteredTodos.map(todo=>(
                         <li key={todo.id}>
                         <h3>{todo.title}</h3>
                      </li>

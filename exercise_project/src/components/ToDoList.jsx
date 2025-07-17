6433724ca1ff592ba41de72531467c2c7a3c9559
Remus Daniel Blaj <remoblaj@gmail.com>
2025-07-17 11:36:12 +0200

    use fetch usato nel componente

--- exercise_project/src/components/ToDoList.jsx
+++ exercise_project/src/components/ToDoList.jsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
+import useFetch from "./useFetch";
 
-const ToDoList = ()=>{
-const [text, setText]= useState("");
-const handleButton = (e)=> {
-    e.preventDefault()
-    setText(e.target.testo.value)
-
-}
-    return(
+const ToDoList = () => {
+    const [text, setText] = useState("");
+    const handleButton = (e) => {
+        e.preventDefault()
+        setText(e.target.testo.value)
+        const [data, loading, error] = useFetch('https://jsonplaceholder.typicode.com/todos');
+        if (loading) 
+            { return <div>Loading...</div> };
+        if (error) 
+            { return <div>Errore di caricamento:{error}</div> };
+    }
+    return (
         <>
-        <h2>To Do List</h2>
-       <form onSubmit={handleButton}>
-        
-        <input type="text" name="testo" />
-        <button>Add</button>
-        
-        </form>
-        
-        <p>{text}</p>
+            <h2>To Do List</h2>
+            <form onSubmit={handleButton}>
+
+                <ul>
+                    {data.map(todo=>(
+                        <li key={ToDoList.id}>
+                        <h3>todo.title</h3>
+                     </li>
+                    ))}                    
+                </ul>
+                <button>Add</button>
+
+            </form>
+
+            <p>{text}</p>
         </>
     )
 };

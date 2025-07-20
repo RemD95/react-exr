93a718c7c5d0e11efd209dc27bd347895e933293
Remus Daniel Blaj <remoblaj@gmail.com>
2025-07-20 15:01:58 +0200

    useMemo

--- exercise_project/src/components/ToDoList.jsx
+++ exercise_project/src/components/ToDoList.jsx
@@ -2,33 +2,42 @@ import { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import useFilteredTodos from "../hooks/useFilteredTodos.js";
 import { useCallback } from "react";
+import { useMemo } from "react";
+
 
 const ToDoList = () => {
     const [text, setText] = useState("");
-    const [searchTerm, setSearchTerm]=usestate("");  
-    
-        const [data, loading, error] = useFetch('https://jsonplaceholder.typicode.com/todos');
-        const filteredTodos = useFilteredTodos(data, searchTerm);
-        const handleSearchChange = useCallback((event) => {
-            setSearchTerm(event.target.value)
-        },[]);
-        if (loading) 
-            { return <div>Loading...</div> };
-        if (error) 
-            { return <div>Errore di caricamento:{error}</div> };
-    
-    
+    const [searchTerm, setSearchTerm] = usestate("");
+
+    const [data, loading, error] = useFetch('https://jsonplaceholder.typicode.com/todos');
+    const filteredTodos = useMemo((data, searchTerm) => {
+        if (!data) {
+            return [];
+        }
+        return data.filter(todo =>
+            todo.title.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+    }, [data, searchTerm]);
+    const handleSearchChange = useCallback((event) => {
+        setSearchTerm(event.target.value)
+    }, []);
+    if (loading) {
+         return <div>Loading...</div> };
+    if (error) { 
+        return <div>Errore di caricamento:{error}</div> };
+
+
     return (
         <>
             <h2>To Do List</h2>
-                <input type="text" onChange={handleSearchChange} />
-                <ul>
-                    {filteredTodos.map(todo=>(
-                        <li key={todo.id}>
+            <input type="text" onChange={handleSearchChange} />
+            <ul>
+                {filteredTodos.map(todo => (
+                    <li key={todo.id}>
                         <h3>{todo.title}</h3>
-                     </li>
-                    ))}                    
-                </ul>
+                    </li>
+                ))}
+            </ul>
             <p>{text}</p>
         </>
     )

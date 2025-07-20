e5fe8bdddff3cd948ecb04308340c8124de6b91c
Remus Daniel Blaj <remoblaj@gmail.com>
2025-07-20 14:40:01 +0200

    useCallback

--- exercise_project/src/components/ToDoList.jsx
+++ exercise_project/src/components/ToDoList.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import useFetch from "../hooks/useFetch";
-import useFilteredTodos from "../hooks/usaFilteredTodos";
+import useFilteredTodos from "../hooks/useFilteredTodos.js";
+import { useCallback } from "react";
 
 const ToDoList = () => {
     const [text, setText] = useState("");
@@ -8,6 +9,9 @@ const ToDoList = () => {
     
         const [data, loading, error] = useFetch('https://jsonplaceholder.typicode.com/todos');
         const filteredTodos = useFilteredTodos(data, searchTerm);
+        const handleSearchChange = useCallback((event) => {
+            setSearchTerm(event.target.value)
+        },[]);
         if (loading) 
             { return <div>Loading...</div> };
         if (error) 
@@ -17,7 +21,7 @@ const ToDoList = () => {
     return (
         <>
             <h2>To Do List</h2>
-                <input type="text" onChange={(event)=>setSearchTerm(event.target.value)} />
+                <input type="text" onChange={handleSearchChange} />
                 <ul>
                     {filteredTodos.map(todo=>(
                         <li key={todo.id}>
